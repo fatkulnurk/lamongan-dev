a1d5e30ff469bd56813ab02864e3c75ee76dd356
fatkulnurk <fatkulnurk@gmail.com>
2023-12-12 13:40:18 +0700

    change structure & add github workflow

---
+++ src/README.md
@@ -0,0 +1,9 @@
+### cara install
+
+```git clone https://github.com/fatkulnurk/lamongan-dev.git```
+
+```cp .env.example .env```
+
+```php artisan migrate:fresh --seed```
+
+```php artisan serve```
---
+++ src/app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json([
+            'message' => 'berhasil mendapatkan data',
+            'data' => User::query()->paginate(10)
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+            'confirm_password' => 'required|same:password',
+        ]);
+
+        $data = collect($validated)->except(['confirm_password'])->toArray();
+        $data['password'] = Hash::make($data['password']);
+        $user = User::query()->create($data);
+        $user->refresh();
+
+        return response()->json([
+            'message' => 'Berhasil menambahkan data',
+            'data' => $user
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $user = User::query()->find($id);
+
+        if (blank($user)) {
+            return response()->json([
+                'message' => 'Data tidak ditemukan!'
+            ], 404);
+        }
+
+        return response()->json([
+            'message' => 'Berhasil mendapatkan data',
+            'data' => $user
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:8',
+            'confirm_password' => 'required|same:password',
+        ]);
+
+        $user = User::query()->findOrFail($id);
+        $data = collect($validated)->except(['confirm_password'])->toArray();
+        $data['password'] = Hash::make($data['password']);
+        $user->update($data);
+        $user->refresh();
+
+        return response()->json([
+            'message' => 'Berhasil mengubah data',
+            'data' => $user
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $user = User::query()->findOrFail($id);
+        $user->delete();
+
+        User::query()->where('id', $id)->delete();
+
+        return response()->json([
+            'message' => 'berhasil menghapus data',
+            'data' => $user
+        ]);
+    }
+}
